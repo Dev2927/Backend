582e95a152e6a2293bac0c28a46c1f4af467af87
dev2927 <devanand290702@gmail.com>
2024-02-04 14:23:03 +0530

    added video controller and updated cloudinary file

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,30 +1,40 @@
-import {v2 as cloudinary} from "cloudinary"
-import fs from "fs"
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
-
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
+// config
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const uploadOnCloudinary = async (localFilePath) => {
-    try {
-        if (!localFilePath) return null
-        //upload the file on cloudinary
-        const response = await cloudinary.uploader.upload(localFilePath, {
-            resource_type: "auto"
-        })
-        // console.log(response)
-        fs.unlinkSync(localFilePath)
-        return response;
+const uploadOnCloudinaary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    // upload the file on cloudinary
+    const responce = await cloudinary.uploader.upload(localFilePath, {
+      folder: "Vtube",
+      resource_type: "auto",
+    });
+    fs.unlinkSync(localFilePath);
+    return responce;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    // remove the local saved tempory file as the upload operation got failed
+    return null;
+  }
+};
 
-    } catch (error) {
-        fs.unlinkSync(localFilePath)
-        return null;
-    }
-}
+const deleteOnCloudinary = async (public_id, resource_type) => {
+  if (!public_id) return null;
+  try {
+    return await cloudinary.uploader.destroy(public_id, {
+      resource_type,
+    });
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
 
-
-
-export {uploadOnCloudinary}
+export { uploadOnCloudinaary, deleteOnCloudinary };
